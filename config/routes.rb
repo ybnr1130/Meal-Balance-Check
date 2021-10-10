62a38f5220f84f66f41af5e581589d90306f0cc9
ybnr1130 <muuckk@gmail.com>
2021-10-10 08:00:54 +0000

    finish routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  # resourcesを使用したルーティング
+  resources :menus, except: [:show]
+  resources :recipes
+  resources :recipe_foods, only: [:new, :create]
+  resources :foods, only: [:create]
+  resources :settings, only: [:index, :edit, :update]
+
+  # settingsコントローラのルーティング
+  get 'settings/:id/delete' => 'settings#delete', as: 'setting_delete'
+  delete 'settings/:id' => 'settings#deleted', as: 'setting_deleted'
+  get 'settings/contact/:id' => 'settings#contact', as: 'contact'
+  post 'settings/contact' => 'settings#send', as: 'send'
+
+  # searchsコントローラのルーティング
+  get 'search' => 'searchs#search', as: 'search'
+
+  # customersコントローラのルーティング
+  get 'customers/new' => 'customers#new', as: 'customers_new'
+  post 'customers' => 'customers#create', as: 'customer'
+
 end
